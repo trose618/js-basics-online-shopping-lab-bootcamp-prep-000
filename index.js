afb0c908647786029c652ed3e25f784dcc562787
Terrance Rose Jr <trose618@gmail.com>
2018-10-01 14:46:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,13 +48,11 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  let updatedCart = []
   for(let i = 0; i < cart.length; i++){
     if(cart[i].itemName===item){
       delete cart[i]
     }
-    updatedCart = cart
-    return updatedCart
+    return cart
   }
 }
 
