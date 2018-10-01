bef3cf9c6215f58c8ff32ee107d32eca273f16ba
Terrance Rose Jr <trose618@gmail.com>
2018-10-01 14:45:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  updatedCart = []
+  let updatedCart = []
   for(let i = 0; i < cart.length; i++){
     if(cart[i].itemName===item){
       delete cart[i]
