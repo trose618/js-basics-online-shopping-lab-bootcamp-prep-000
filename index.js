54da013a5e12eae46f0403d31ef32fb409e0a455
Terrance Rose Jr <trose618@gmail.com>
2018-10-01 14:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for(let i = 0; i < cart.length; i++){
+    if(cart[i].itemName===item){
+      delete cart[i]
+    }
+    return cart
+  }
 }
 
 function placeOrder(cardNumber) {
