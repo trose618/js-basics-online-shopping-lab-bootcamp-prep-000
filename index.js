ff6d0558712ebda38e5014ed8471e25cbdf1f694
Terrance Rose Jr <trose618@gmail.com>
2018-10-01 15:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,5 +60,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber===null){
     console.log("Sorry, we don't have a credit card on file for you.")
+  }else{
+    
   }
 }
