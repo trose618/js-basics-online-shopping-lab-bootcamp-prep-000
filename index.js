e2c8c03b1b33920b159eaced88419d2df1846a18
Terrance Rose Jr <trose618@gmail.com>
2018-10-01 15:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ function placeOrder(cardNumber) {
   if(cardNumber===null){
     console.log("Sorry, we don't have a credit card on file for you.")
   }else{
-    
+    receipt = `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 }
